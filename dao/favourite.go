613a81d6bf850f339731db794b006de11c34b61a
Token Token Token <donghui.wang@ucdconnect.ie>
2023-01-30 00:43:11 +0000

    看完了jwcen的框架在准备实现favourite分配的两个功能
    目前DAO和Service层Favourite接口要实现的功能已经写了个框架，近日完成。在jwcen代码上增添的部分增加了注释。

--- dao/favourite.go
+++ dao/favourite.go
@@ -6,17 +6,30 @@ import (
 )
 
 // 点赞
-func GetFavourite(u *models.User, video *models.Video) bool {
-	fav := &models.Favourite{}
+// 由于业务层需要增加一个error返回值
+func GetFavourite(u *models.User, video *models.Video) (bool, error) {
+	fav := &models.Favourite{} //favorite
 	err := common.GetDB().
 		Where("user_id=? and video_id =?", u.ID, video.ID).
 		First(fav).Error
 	if err != nil {
-		return false
+		return false, err
 	}
 	if fav.UserId == 0 || fav.IsFavourite == 0 {
-		return false
+		return false, err
 	}
-	return true
+	return true, err
 
 }
+
+// DAO层通过ID查询用户赞过视频的空函数
+func GetFavouriteVideoListByUserId(userId int64) ([]int64, error) {
+	var favouriteList []int64
+	return favouriteList, nil
+}
+
+// 点赞功能空函数
+func AddFavourite() {}
+
+// 取消功能空函数
+func CancelFavourite() {}
