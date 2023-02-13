4bdc10af5e59775e9ef36d381a936c6819158c41
Token Token Token <donghui.wang@ucdconnect.ie>
2023-02-13 15:06:43 +0000

    消报红 删了没用的注释

--- dao/favourite.go
+++ dao/favourite.go
@@ -2,10 +2,8 @@ package dao
 
 import (
 	"errors"
-	"log"
 	"titok_v1/common"
 	"titok_v1/models"
-	// "gorm"
 )
 
 type Favourite struct {
@@ -16,6 +14,7 @@ type Favourite struct {
 
 // 点赞
 // 由于业务层需要增加一个error返回值
+// 这里把数据库表中的0和1映射成true或者false
 func GetFavourite(u *models.User, video *models.Video) (bool, error) {
 	fav := &models.Favourite{} //favorite
 	err := common.GetDB().
@@ -30,34 +29,12 @@ func GetFavourite(u *models.User, video *models.Video) (bool, error) {
 	return true, err
 }
 
-// DAO层通过ID查询用户赞过视频的空函数
-
-// 这里需要数据库的.sql 在本地试一试  Favourite的表是什么样的？
-func GetFavouriteVideoListByUserId(userId int64) ([]int64, error) {
-	var favouriteList []int64
-	err := common.DB.Model(Favourite{}).Where(map[string]interface{}{"user_id": userId}).Pluck("video_id", &favouriteList).Error
+func GetFavouriteVideoListByUserId(userId int64) ([]models.Favourite, error) {
+	var favouriteSet []models.Favourite
+	err := common.GetDB().Where("user_id = ? and is_favourite = 1").Find(&favouriteSet)
 
 	if err != nil {
-		log.Printf(err.Error())
-		return nil, errors.New("Method GetFavouriteeVideoListByUserId failed")
-	} else {
-		return favouriteList, nil
+		return nil, errors.New("GetFavouritedVideoListByUserId failed." + err.Error.Error())
 	}
-	//这里没有测试 参照他人项目修改了函数 但是目前不确定
-	//我觉得这个功能的DAO还要构思一下 近日会把文档发出来讨论
+	return favouriteSet, nil
 }
-
-
-// 初步在DAO层写了通过用户ID返回点赞列表的方法
-// func FavouriteAction(token string, videoId int64,actionType bool)([]int64 ,err){
-// 	var favouriteSet []models.Favourite
-// 	if result := DB.Select("video_id","is_favourite").Model(&models.Favourite{}).Where("user_id = ?", userId).Find(&favouriteSet);
-// 	result.Error! = nil{
-// 		return nil, result.Error
-// 	}
-
-// 	favouriteList = make([]int64, 0, len(favouriteSet))
-// 	for _, each := range favouriteSet{
-// 		favouriteList = append(favouriteSet, each.videoId)
-// 	}
-// }
