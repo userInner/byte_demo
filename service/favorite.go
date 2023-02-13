ac924f19f1df8a12c9522bac25debabeb55e2583
Token Token Token <donghui.wang@ucdconnect.ie>
2023-02-13 15:08:14 +0000

    消报红 删除无用注释 点赞功能按接口文档实现返回 点赞列表未实现该功能

--- service/favorite.go
+++ service/favorite.go
@@ -1,86 +1,107 @@
 package service
 
-// import (
-// 	// "gorm"
-// 	"log"
-// 	"titok_v1/dao"
-// 	"titok_v1/models"
-// )
+import (
+	"log"
+	"titok_v1/common"
+	"titok_v1/dao"
+	"titok_v1/models"
+	resp "titok_v1/response"
+)
 
-// type favouriteService struct {
-// 	user  models.User
-// 	video models.Video
-// }
+type favouriteService struct {
+	user  models.User
+	video models.Video
+}
 
-// // 初步的getFavouriteStatus方法
-// func (f *favouriteService) GetFavouriteStatus(userId int64, videoId int64) (bool, error) {
-// 	favouriteStatus, err := dao.GetFavourite(&f.user, &f.video)
-// 	if err == nil {
-// 		return favouriteStatus, nil
-// 	} else {
-// 		log.Printf("Method getFavoriteStatus failed :%v", err)
-// 		return false, err
-// 	}
-// }
+// 初步的getFavouriteStatus方法
+func (f *favouriteService) GetFavouriteStatus(userId int64, videoId int64) bool {
+	favouriteStatus, err := dao.GetFavourite(&f.user, &f.video)
+	if err == nil {
+		return favouriteStatus
+	} else {
+		log.Printf("Method getFavoriteStatus failed :%v", err)
+		return false
+	}
+}
 
-// // 点赞的操作 根据传入的actionType区分是点赞和取消
-// func (f *favouriteService) FavoriteAction(userId int64, videoId int64, actionType int32) (json, error) {
+// 点赞的操作 根据传入的actionType区分是点赞和取消
+func (f *favouriteService) FavoriteAction(userId int64, videoId int64, actionType int32) *resp.FavouriteActionResp {
 
-// 	flag, err := GetFavouriteStatus(userId, videoId)
-// 	var status_msg string
-// 	var status_code int32
+	flag := f.GetFavouriteStatus(userId, videoId)
 
-// 	if actionType == 1 {
-// 		if flag == true {
-// 			c.JSON(http.status_code, http.status_msg)
-// 			return true, nil
-// 		} else {
-// 			var favourite models.favourite
+	if actionType == 1 {
+		if flag == true {
+			return &resp.FavouriteActionResp{
+				StatusCode: 0,
+				StatusMsg:  "successed",
+			}
+		} else {
 
-// 			favourite.UserId = userId
-// 			favourite.VideoId = videoId
-// 			favourite.IsFavourite = true
-// 			c.JSON(http.status_code, http.status_msg)
+			var favourite models.Favourite
+			favourite.UserId = userId
+			favourite.VideoId = videoId
+			favourite.IsFavourite = 1
+			if err := common.DB.Create(&favourite).Error; err != nil {
+				log.Printf("Insert favourite log failed :%v", err)
+				return &resp.FavouriteActionResp{
+					StatusCode: -1,
+					StatusMsg:  "failed",
+				}
+			}
+		}
+	}
+	if actionType == 0 {
+		if flag == false {
+			return &resp.FavouriteActionResp{
+				StatusCode: 0,
+				StatusMsg:  "successed",
+			}
+		} else {
 
-// 			if err := DB.Create(&favourite).Error; err != nil {
-// 				log.Printf("Insert favourite log failed :%v", err)
-// 				c.JSON(http.status_code, http.status_msg)
-// 				return false, err
-// 			}
-// 		}
-// 	}
-// 	if actionType == 0 {
-// 		if flag == false {
-// 			c.JSON(http.status_code, http.status_msg)
-// 			return true, nil
-// 		} else {
+			var favourite models.Favourite
+			favourite.UserId = userId
+			favourite.VideoId = videoId
+			favourite.IsFavourite = 1
 
-// 			var favourite models.favourite
-// 			favourite.UserId = userId
-// 			favourite.VideoId = videoId
-// 			favourite.IsFavourite = true
+			if err := common.DB.Where("user_id = ? and video_id = ?", userId, videoId).Update("IsFavourite", 0); err != nil {
+				log.Printf("Delete favourite log failed :%v", err)
+				return &resp.FavouriteActionResp{
+					StatusCode: -1,
+					StatusMsg:  "failed",
+				}
+			}
+		}
+	}
 
-// 			//这里应该要再改一下
-// 			if err := DB.Where("user_id = ? and video_id = ?", userId, videoId).Delete(); err != nil {
-// 				log.Printf("Delete favourite log failed :%v", err)
+	return &resp.FavouriteActionResp{
+		StatusCode: 0,
+		StatusMsg:  "successed",
+	}
 
-// 				c.JSON(http.status_code, http.status_msg)
-// 				return false, err
-// 			}
-// 		}
-// 	}
+	//点赞逻辑：查询列表用户是否已经点赞，如果已经点赞则不修改，如果未点赞则加入。
+}
 
-// 	return true, nil //默认返回
+// 在DAO层实现了 这里直接调用DAO层的函数
+func GetFavouriteVideoListByUserId(userId int64) []models.Favourite {
+	favouriteList, err := dao.GetFavouriteVideoListByUserId(userId)
+	if err != nil {
+		log.Printf("Get Favouritee Video List By UserID failed :%v", err)
+		return nil
+	}
 
-// 	//点赞逻辑：查询列表用户是否已经点赞，如果已经点赞则不修改，如果未点赞则加入。
-// }
+	//从video_id中拿author信息
 
-// // 在DAO层实现了 这里直接调用DAO层的函数
-// func GetFavouriteVideoListByUserId(userId int64) []int64 {
-// 	favouriteList, err := dao.GetFavouriteVideoListByUserId(userId)
-// 	if err != nil {
-// 		log.Printf("Get Favouritee Video List By UserID failed :%v", err)
-// 		return err
-// 	}
-// 	return favouriteList
-// }
+	//从video_id中拿play_url, cover_url, 点赞数，评论数和标题。
+
+	//要用GetCover
+
+	//要用GetMinioUrl
+
+	//拿video的Favourite Count
+
+	//拿video的标题
+
+	//拿Video的评论数 没有方法要写
+
+	return favouriteList
+}
